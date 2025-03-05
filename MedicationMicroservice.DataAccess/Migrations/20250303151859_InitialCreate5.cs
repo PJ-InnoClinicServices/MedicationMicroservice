cf2a10d234bd98839e29d38dea78226c2e81d674
piotrjass <131187708+piotrjass@users.noreply.github.com>
2025-03-05 15:47:17 +0100

    Add gitignore, migrations, packages

---
+++ MedicationMicroservice.DataAccess/Migrations/20250303151859_InitialCreate5.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MedicationMicroservice.Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate5 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Diseases",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Diseases", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Drugs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Manufacturer = table.Column<string>(type: "text", nullable: false),
+                    Price = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Drugs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Substances",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    SubstanceName = table.Column<string>(type: "text", nullable: false),
+                    Dosage = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Substances", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DiseaseSubstance",
+                columns: table => new
+                {
+                    DiseasesId = table.Column<Guid>(type: "uuid", nullable: false),
+                    SubstancesId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DiseaseSubstance", x => new { x.DiseasesId, x.SubstancesId });
+                    table.ForeignKey(
+                        name: "FK_DiseaseSubstance_Diseases_DiseasesId",
+                        column: x => x.DiseasesId,
+                        principalTable: "Diseases",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DiseaseSubstance_Substances_SubstancesId",
+                        column: x => x.SubstancesId,
+                        principalTable: "Substances",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DrugSubstance",
+                columns: table => new
+                {
+                    DrugsId = table.Column<Guid>(type: "uuid", nullable: false),
+                    SubstancesId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DrugSubstance", x => new { x.DrugsId, x.SubstancesId });
+                    table.ForeignKey(
+                        name: "FK_DrugSubstance_Drugs_DrugsId",
+                        column: x => x.DrugsId,
+                        principalTable: "Drugs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DrugSubstance_Substances_SubstancesId",
+                        column: x => x.SubstancesId,
+                        principalTable: "Substances",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DiseaseSubstance_SubstancesId",
+                table: "DiseaseSubstance",
+                column: "SubstancesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DrugSubstance_SubstancesId",
+                table: "DrugSubstance",
+                column: "SubstancesId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DiseaseSubstance");
+
+            migrationBuilder.DropTable(
+                name: "DrugSubstance");
+
+            migrationBuilder.DropTable(
+                name: "Diseases");
+
+            migrationBuilder.DropTable(
+                name: "Drugs");
+
+            migrationBuilder.DropTable(
+                name: "Substances");
+        }
+    }
+}
